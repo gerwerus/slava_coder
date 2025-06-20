9707780b90b83cd210f3ae5889ed3c63bd663875
Vyachealav <snikolaev000@mail.ru>
2025-06-20 13:02:47 +0300

    fix

--- src/cycles/zadanie2.py
+++ src/cycles/zadanie2.py
@@ -1,7 +1,7 @@
 # Написать программу, которая выводит массив чисел от 1 до N. И выводит сумму всех чисел этого массива.
 # Число N спрашивается у пользователя из консоли. https://i.imgur.com/yr6J6xv.
 number = int(input('Введите число n: '))
-massive = list(range(1,number + 1))
+massive = list(range(1, number + 1))
 print(massive)
 sum = sum(massive)
 print(f'Сумма чисел: {sum}')
