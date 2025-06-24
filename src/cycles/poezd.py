6277ebbc369bf66a53e1cdfe3fa9ca48b71c0505
Vyachealav <snikolaev000@mail.ru>
2025-06-24 16:09:40 +0300

    yes fix for mr

--- src/cycles/poezd.py
+++ src/cycles/poezd.py
@@ -11,117 +11,29 @@
 # значит следующий билет должен быть с номером 3.
 # https://i.imgur.com/6D9dgBR.png
 
+from const.poezd_const import base_pavlovsk, base_vyborg, base_kronstadt
+
 import random
 
 
-def base_append_pavlovsk():
-    base_details = []
-    base_details.append(ticket_number)
-    base_details.append(route_name)
-    base_details.append(username_fio)
-    base_details.append(pasport_details)
-    base_pavlovsk[place_key] = base_details
+def base_append(base, base_pavlovsk, base_vyborg, base_kronstadt):
+    base['Билеты в Павлоск'] = base_pavlovsk
+    base['Билеты в Выборг'] = base_vyborg
+    base['Билеты в Крондштат'] = base_kronstadt
 
 
-def base_append_vyborg():
-    base_details = []
-    base_details.append(ticket_number)
-    base_details.append(route_name)
-    base_details.append(username_fio)
-    base_details.append(pasport_details)
-    base_vyborg[place_key] = base_details
-
-
-def base_append_kronstadt():
-    base_details = []
-    base_details.append(ticket_number)
-    base_details.append(route_name)
-    base_details.append(username_fio)
-    base_details.append(pasport_details)
-    base_kronstadt[place_key] = base_details
-
-
-def base_print_pavlosk():
-    for key, values in base_pavlovsk.items():
+def base_print(base):
+    for key, value in base.items():
         print(key)
-        for value in values:
-            print(value)
+        for key2, value2 in value.items():
+            print(key2)
+            for value3 in value2:
+                print(value3)
 
 
-def base_print_vyborg():
-    for key, values in base_vyborg.items():
-        print(key)
-        for value in values:
-            print(value)
+base = {}
 
 
-def base_print_kronstadt():
-    for key, values in base_kronstadt.items():
-        print(key)
-        for value in values:
-            print(value)
-
-
-base_pavlovsk = {
-    'Место №1': [
-        'Билет №7593096766601',
-        'Маршрут: Saint-Petersburg -> Pavlovsk',
-        'Пассажир: Петров Н.Е.',
-        'Паспорт: 23 23 434343',
-    ],
-    'Место №2': [
-        'Билет №7593096766602',
-        'Маршрут: Saint-Petersburg -> Pavlovsk',
-        'Пассажир: Куличев Н.Е.',
-        'Паспорт: 24 24 444444',
-    ],
-    'Место №3': [
-        'Билет №7593096766603',
-        'Маршрут: Saint-Petersburg -> Pavlovsk',
-        'Пассажир: Улуханов Н.Е.',
-        'Паспорт: 25 25 454545',
-    ],
-}
-base_vyborg = {
-    'Место №1': [
-        'Билет №7593096766601',
-        'Маршрут: Saint-Petersburg -> Vyborg',
-        'Пассажир: Петров Н.Е.',
-        'Паспорт: 23 23 434343',
-    ],
-    'Место №2': [
-        'Билет №7593096766602',
-        'Маршрут: Saint-Petersburg -> Vyborg',
-        'Пассажир: Куличев Н.Е.',
-        'Паспорт: 24 24 444444',
-    ],
-    'Место №3': [
-        'Билет №7593096766603',
-        'Маршрут: Saint-Petersburg -> Vyborg',
-        'Пассажир: Улуханов Н.Е.',
-        'Паспорт: 25 25 454545',
-    ],
-}
-base_kronstadt = {
-    'Место №1': [
-        'Билет №7593096766601',
-        'Маршрут: Saint-Petersburg -> Kronstadt',
-        'Пассажир: Петров Н.Е.',
-        'Паспорт: 23 23 434343',
-    ],
-    'Место №2': [
-        'Билет №7593096766602',
-        'Маршрут: Saint-Petersburg -> Kronstadt',
-        'Пассажир: Куличев Н.Е.',
-        'Паспорт: 24 24 444444',
-    ],
-    'Место №3': [
-        'Билет №7593096766603',
-        'Маршрут: Saint-Petersburg -> Kronstadt',
-        'Пассажир: Улуханов Н.Е.',
-        'Паспорт: 25 25 454545',
-    ],
-}
 choice = input('Добро пожаловать, хотите приобрести билет? ').lower()
 while True:
     if choice == 'да':
@@ -138,8 +50,11 @@ while True:
                 username_fio = f'Пассажир: {input("Введите фамилию и инициалы в формате: Фамилия И.О.\n")}'
                 pasport_details = f'Паспорт: {input("Укажите серию и номер паспорта в формате: XX XX XXXXXX\n")}'
                 ticket_number = f'Билет №{random.randint(7593096766600, 7593096766700)}'
-                base_append_pavlovsk()  # СОЗДАНИЕ МАССИВА С ДАННЫМИ, И ЕГО ДОБАВЛЕНИЕ В БАЗУ ВЫБРАННОГО МАРШРУТА
-                base_print_pavlosk()  # ВЫВОД В СТОЛБИК ДАННЫХ О БИЛЕТЕ
+                base_pavlovsk[place_key] = [ticket_number, route_name, username_fio, pasport_details]
+                base_append(
+                    base, base_pavlovsk, base_vyborg, base_kronstadt
+                )  # СОЗДАНИЕ МАССИВА С ДАННЫМИ, И ЕГО ДОБАВЛЕНИЕ В БАЗУ ВЫБРАННОГО МАРШРУТА
+                base_print(base)  # ВЫВОД В СТОЛБИК ДАННЫХ О БИЛЕТЕ
                 break
             else:
                 print(f'{place_key} занято')
@@ -153,9 +68,13 @@ while True:
                 username_fio = f'Пассажир: {input("Введите фамилию и инициалы в формате: Фамилия И.О.\n")}'
                 pasport_details = f'Паспорт: {input("Укажите серию и номер паспорта в формате: XX XX XXXXXX\n")}'
                 ticket_number = f'Билет №{random.randint(7593096766600, 7593096766700)}'
-                base_append_vyborg()  # СОЗДАНИЕ МАССИВА С ДАННЫМИ, И ЕГО ДОБАВЛЕНИЕ В БАЗУ ВЫБРАННОГО МАРШРУТА
-                base_print_vyborg()  # ВЫВОД В СТОЛБИК ДАННЫХ О БИЛЕТЕ
+                base_vyborg[place_key] = [ticket_number, route_name, username_fio, pasport_details]
+                base_append(
+                    base, base_pavlovsk, base_vyborg, base_kronstadt
+                )  # СОЗДАНИЕ МАССИВА С ДАННЫМИ, И ЕГО ДОБАВЛЕНИЕ В БАЗУ ВЫБРАННОГО МАРШРУТА
+                base_print(base)  # ВЫВОД В СТОЛБИК ДАННЫХ О БИЛЕТЕ
                 break
+
             else:
                 print(f'{place_key} занято')
 
@@ -168,9 +87,14 @@ while True:
                 username_fio = f'Пассажир: {input("Введите фамилию и инициалы в формате: Фамилия И.О.\n")}'
                 pasport_details = f'Паспорт: {input("Укажите серию и номер паспорта в формате: XX XX XXXXXX\n")}'
                 ticket_number = f'Билет №{random.randint(7593096766600, 7593096766700)}'
-                base_append_kronstadt()  # СОЗДАНИЕ МАССИВА С ДАННЫМИ, И ЕГО ДОБАВЛЕНИЕ В БАЗУ ВЫБРАННОГО МАРШРУТА
-                base_print_kronstadt()  # ВЫВОД В СТОЛБИК ДАННЫХ О БИЛЕТЕ
+                base_kronstadt[place_key] = [ticket_number, route_name, username_fio, pasport_details]
+                base_append(
+                    base, base_pavlovsk, base_vyborg, base_kronstadt
+                )  # СОЗДАНИЕ МАССИВА С ДАННЫМИ, И ЕГО ДОБАВЛЕНИЕ В БАЗУ ВЫБРАННОГО МАРШРУТА
+                base_print(base)  # ВЫВОД В СТОЛБИК ДАННЫХ О БИЛЕТЕ
                 break
+            else:
+                print(f'{place_key} занято')
     else:
         print('До свидания!')
         break
