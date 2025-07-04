1e79da576ab001f75b8b179c6e3879dfe6b79cf4
Vyachealav <snikolaev000@mail.ru>
2025-07-04 14:07:20 +0300

    dual_currency

--- src/function_with_massive_and_string/dual_currency.py
+++ src/function_with_massive_and_string/dual_currency.py
@@ -9,3 +9,20 @@
 # Опять же, с точностью до копеек или центов.
 # https://i.imgur.com/haSYAti.png
 
+
+def dual_currency_count(exchange_rate, current_rubles, current_dollars):
+    transfer_to_rubles = current_dollars * exchange_rate  # перевод из долларов в рубли
+    average = (transfer_to_rubles + current_rubles) / 2
+    diff = abs(average - current_rubles)
+    if transfer_to_rubles > current_rubles:
+        print(f'Вам надо продать доллары на {diff} рублей')
+    elif transfer_to_rubles < current_rubles:
+        print(f'Вам надо докупить доллары на {diff} рублей')
+    else:
+        print('Ваш портфель сбалансирован')
+
+
+exchange_rate = float(input('Курс доллара:\n'))
+current_rubles = float(input('Сколько у вас рублей?\n'))
+current_dollars = float(input('Сколько у вас долларов?\n'))
+dual_currency_count(exchange_rate, current_rubles, current_dollars)
